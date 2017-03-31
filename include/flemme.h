262ac7b1fd862641c4c30c6b06820d9aa80af888
Jean Pignouf <maxime.de-la-fouchardiere@epitech.eu>
2017-03-31 16:19:57 +0100

    Removed folders

---
+++ include/flemme.h
@@ -0,0 +1,16 @@
+/*
+** flemme.h for lib in /home/ratouney/lib/include
+** 
+** Made by 
+** Login   <ratouney>
+** 
+** Started on  Fri Mar 31 16:18:14 2017 
+** Last update Fri Mar 31 16:19:11 2017 
+*/
+
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
