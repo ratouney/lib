b92ff7b5e85aff88dd0eddf7411c70fcb081550f
Jean Pignouf <maxime.de-la-fouchardiere@epitech.eu>
2017-04-03 20:22:35 +0100

    correciton de headers variées

--- include/flemme.h
+++ include/flemme.h
@@ -5,12 +5,21 @@
 ** Login   <ratouney>
 ** 
 ** Started on  Fri Mar 31 16:18:14 2017 
-** Last update Fri Mar 31 16:19:11 2017 
+** Last update Mon Apr  3 20:22:02 2017 
 */
 
-#include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/stat.h>
+#ifndef FLEMME_H_
+# define FLEMME_H_
+
+# include <stdlib.h>
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include "calib.h"
+# include "get_next_line.h"
+# include "loadfile.h"
+# include "ratlib.h"
+
+#endif /* !FLEMME_H_ */
--- string/my_strlen.c
+++ string/my_strlen.c
@@ -5,7 +5,7 @@
 ** Login   <maxime.de-la-fouchardiere@epitech.eu>
 ** 
 ** Started on  Mon Mar 20 16:16:56 2017 
-** Last update Tue Mar 28 19:02:22 2017 
+** Last update Mon Apr  3 20:20:35 2017 
 */
 
 #include <unistd.h>
@@ -16,7 +16,7 @@ int	my_strlen(char *str)
 
   count = 0;
   if (str == NULL)
-    return (-1);
+    return (0);
   while (str[count] != '\0')
     count++;
   return (count);
