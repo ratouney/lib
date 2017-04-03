10f382190b75bea874de1dee196ada7135328cd3
Jean Pignouf <maxime.de-la-fouchardiere@epitech.eu>
2017-04-03 20:29:02 +0100

    correciton de headers variées

--- include/loadfile.h
+++ include/loadfile.h
@@ -5,7 +5,7 @@
 ** Login   <ratouney>
 ** 
 ** Started on  Mon Apr  3 14:08:58 2017 
-** Last update Mon Apr  3 14:09:29 2017 
+** Last update Mon Apr  3 20:28:37 2017 
 */
 
 #ifndef LOADFILE_H_
@@ -13,24 +13,28 @@
 
 typedef struct s_content
 {
-    char *raw;
-    char **line;
+    char	*raw;
+    char	**line;
 
-    int errno;
-    int errno_mod;
-    int fd;
-    int size;
-} t_content;
+    int		errno;
+    int		errno_mod;
+    int		fd;
+    int		size;
+}		t_content;
 
-void loadfile_destroy(t_content *data);
-void loadfile_show(t_content *data);
-void loadfile_showerr(t_content *data);
-int loadfile_sep(char *str, char c);
-int main(int argc, char **argv);
-t_content *loadfile_line_prep(t_content *info);
-t_content *loadfile_line(t_content *info);
-t_content *loadfile(char *path);
-char *loadfile_raw(int fd);
+void		loadfile_destroy(t_content *data);
+void		loadfile_show(t_content *data);
+void		loadfile_showerr(t_content *data);
+int		loadfile_sep(char *str, char c);
+t_content       *loadfile_line_prep(t_content *info);
+t_content	*loadfile_line(t_content *info);
+t_content       *loadfile(char *path);
+char		*loadfile_raw(int fd);
+
+# define LF_LOD(x) loadfile(x);
+# define LF_ERR(x) loadfile_showerr(x);
+# define LF_DES(x) loadfile_destroy(x);
+# define LF_SHW(x) loadfile_show(x);
 
 #endif /* !LOADFILE_H_ */
 
